40839ebc69062e75e5c85eee45f8dbc10992c708
cloveropen.com <cloveropen@163.com>
2019-08-13 15:02:31 +0800

    add login

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container fluid class="pa-0">
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+    <v-layout wrap>
+      <v-flex xs12>
+        <p>Normal</p>
+      </v-flex>
+
+      <v-flex xs12 sm3>
+        <v-btn text icon color="pink">
+          <v-icon>favorite</v-icon>
+        </v-btn>
+      </v-flex>
+
+      <v-flex xs12 sm3>
+        <v-btn text icon color="indigo">
+          <v-icon>star</v-icon>
+        </v-btn>
+      </v-flex>
+
+      <v-flex xs12 sm3>
+        <v-btn text icon color="green">
+          <v-icon>cached</v-icon>
+        </v-btn>
+      </v-flex>
+
+      <v-flex xs12 sm3>
+        <v-btn text icon color="deep-orange">
+          <v-icon>thumb_up</v-icon>
+        </v-btn>
+      </v-flex>
+       <v-icon>mdi-anchor</v-icon>
+
+    <v-icon>thumb_up</v-icon>
+
+    <v-icon>cached</v-icon>
+
+    <v-icon>mdi-tilde</v-icon>
+
+    <v-icon>favorite</v-icon>
+
+    <v-icon>mdi-mouse</v-icon>
+    </v-layout>
+  </v-container>
 </template>
