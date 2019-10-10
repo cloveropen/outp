97781ac4e4d831a17f50777aafe20d630b700727
cloveropen.com <cloveropen@163.com>
2019-10-10 08:58:15 +0800

    门急诊挂号收款主页面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <v-layout justify-start row wrap>
         <v-flex d-flex>
           <div class="ih-item square colored effect9 bottom_to_top">
-            <a href="#">
+            <router-link :to="{name:'outreg'}">
               <div class="img">
                 <img :src="require('../assets/img/reg.jpg')" alt="img" />
               </div>
@@ -14,15 +14,13 @@
                   <h3>门诊挂号</h3>
                   <p>扫病历条码挂号或直接挂号,打印挂号单</p>
                 </div>
-              </div></a
-            >
+              </div>
+            </router-link>
           </div>
         </v-flex>
-
         <v-flex d-flex>
-          <!-- colored -->
           <div class="ih-item square colored effect9 bottom_to_top">
-            <a href="#">
+            <router-link :to="{name:'outcash'}">
               <div class="img">
                 <img :src="require('../assets/img/cash.jpg')" alt="img" />
               </div>
@@ -31,13 +29,13 @@
                   <h3>划价收款</h3>
                   <p>接收门诊医生工作站申请单和处方,收款打印收据</p>
                 </div>
-              </div></a
-            >
+              </div>
+            </router-link>
           </div>
         </v-flex>
         <v-flex d-flex>
           <div class="ih-item square colored effect9 bottom_to_top">
-            <a href="#">
+            <router-link :to="{name:'outchk'}">
               <div class="img">
                 <img :src="require('../assets/img/chk.jpg')" alt="img" />
               </div>
@@ -46,13 +44,13 @@
                   <h3>交班结算</h3>
                   <p>交班统计本班工作量和收款额、收据张数及起止流水号</p>
                 </div>
-              </div></a
-            >
+              </div>
+            </router-link>
           </div>
         </v-flex>
         <v-flex d-flex>
           <div class="ih-item square colored effect9 left_to_right">
-            <a href="#">
+            <router-link :to="{name:'outreceipt'}">
               <div class="img">
                 <img :src="require('../assets/img/receipt.jpg')" alt="img" />
               </div>
@@ -61,38 +59,38 @@
                   <h3>发票号管理</h3>
                   <p>领用挂号、收款、预交金收据登记和流水号设置</p>
                 </div>
-              </div></a
-            >
+              </div>
+            </router-link>
           </div>
         </v-flex>
         <v-flex d-flex>
           <div class="ih-item square effect9 left_to_right">
-            <a href="#">
+            <router-link :to="{name:'detailreg'}">
               <div class="img">
                 <img :src="require('../assets/img/reg_detail.jpg')" alt="img" />
               </div>
               <div class="info">
                 <div class="info-back">
-                  <h3>挂号明细</h3>
-                  <p>一段时间内挂号费用明细和支付明细</p>
+                  <h3>查询挂号明细</h3>
+                  <p>挂号费用明细和支付明细</p>
                 </div>
-              </div></a
-            >
+              </div>
+            </router-link>
           </div>
         </v-flex>
         <v-flex d-flex>
           <div class="ih-item square colored effect9 left_to_right">
-            <a href="#">
+            <router-link :to="{name:'detailcash'}">
               <div class="img">
                 <img :src="require('../assets/img/cash_detail.jpg')" alt="img" />
               </div>
               <div class="info">
                 <div class="info-back">
-                  <h3>收款明细</h3>
-                  <p>一段时间内收款费用明细和结算明细</p>
+                  <h3>查询收款明细</h3>
+                  <p>收款费用明细和每次结算明细</p>
                 </div>
-              </div></a
-            >
+              </div>
+            </router-link>
           </div>
         </v-flex>
         <v-flex d-flex>
@@ -127,7 +125,7 @@
         </v-flex>
         <v-flex d-flex>
           <div class="ih-item square colored effect9 top_to_bottom">
-            <a href="#">
+            <router-link :to="{name:'detailundo'}">
               <div class="img">
                 <img :src="require('../assets/img/undo.jpg')" alt="img" />
               </div>
@@ -136,8 +134,8 @@
                   <h3>退款明细</h3>
                   <p>退号退款记录</p>
                 </div>
-              </div></a
-            >
+              </div>
+            </router-link>
           </div>
         </v-flex>
       </v-layout>
